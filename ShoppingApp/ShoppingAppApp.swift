930d915fcf0e8309fbb545a13af1b53a450ced4d
mertcancifter <mert_1998_03@hotmail.com.tr>
2022-11-23 23:03:58 +0300

    Initial Commit

---
+++ ShoppingApp/ShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppApp.swift
+//  ShoppingApp
+//
+//  Created by mert can çifter on 23.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
